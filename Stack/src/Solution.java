839f14c5691a2db4f4d0f887896259f5ddcf8a5f
David <qiankunli@yahoo.com>
2018-09-15 20:53:33 +0800

    clean up

--- Stack/src/Solution.java
+++ Stack/src/Solution.java
@@ -1,16 +1,11 @@
-import java.util.Stack;
-
 public class Solution {
     public boolean isValid(String string){
-        Stack<Character> stack = new Stack<>();
+        ArrayStack<Character> stack = new ArrayStack<>();
         for (int i = 0; i < string.length(); i ++) {
             char c = string.charAt(i);
-            System.out.print(c);
             if (c == '(' || c == '[' || c == '{')
                 stack.push(c);
              else {
-                System.out.print("else");
-                System.out.print(c);
                 if (stack.isEmpty())
                     return false;
                 char topChar = stack.pop();
